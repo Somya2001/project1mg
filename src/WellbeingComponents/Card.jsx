512eb11fb9f394e7b6f233f98d7b4333e0345080
Somya <somya200111@gmail.com>
2022-05-30 00:36:52 +0530

    first commit

---
+++ src/WellbeingComponents/Card.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import React, { useState } from "react";
+import styles from "./card.module.css";
+
+export const Card = ({ item, getNumber}) => {
+  const { discount, form, image, mrp, name, quantity, sp, uses, _id } = item;
+
+  const [ADD, setADD] = useState("ADD");
+
+  const handelClick = async () => {
+    await setTimeout(() => {
+      setADD("ADDIND...");
+    }, 100);
+
+    await setTimeout(() => {
+      setADD("✔ ADDED");
+    }, 1200);
+
+    await setTimeout(() => {
+      setADD("ADD");
+    }, 2000);
+
+    getNumber();
+        const obj = {...item, numberOfItems: 1};
+        console.log(obj);
+        const post = await axios.post(
+			"https://nodejsbackendtatamg.herokuapp.com/cart",
+			obj
+		);
+        console.log("Post to cart", post);
+    }
+    
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={image} alt="" />
+      </div>
+      <div className={styles.detailContainer}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.quantity}>{quantity}</p>
+        <p className={styles.price}>
+          MRP <span className={styles.mrp}>₹{mrp}</span>{" "}
+          <span className={styles.discount}>{discount}% OFF</span>
+        </p>
+        <div className={styles.bottom}>
+          <span className={styles.sp}>₹{sp}</span>
+          <span onClick={handelClick} className={styles.add}>
+            {ADD}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
